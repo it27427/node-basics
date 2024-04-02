689dd58daef57cbc9e42f5dcc3968471ed0f48c7
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-02 20:07:20 +0600

    first commit

---
+++ 01-node-modules/index.js
@@ -0,0 +1,6 @@
+const test = require('./test');
+
+console.log(test);
+
+test.getName();
+test.getAge();
---
+++ 01-node-modules/test.js
@@ -0,0 +1,9 @@
+const getName = () => {
+  return 'Istiak Hossain Tushar';
+};
+
+const getAge = () => {
+  return 31;
+};
+
+module.exports = { getName, getAge };
