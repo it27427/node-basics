7201f9960d0c6d7e5df43e1302d236c31cec8fb6
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-03 10:53:44 +0600

    userInfo, homedir, hostname, totalmem and freemem get from OS Module

---
+++ 01-node-built-in-modules/osandpath.js
@@ -0,0 +1,17 @@
+// OS MODULE
+const os = require('os');
+
+// GET USER-INFO by userInfo() Module
+console.log(os.userInfo());
+
+// GET HOME-DIRECTORY by homedir() Module
+console.log(os.homedir());
+
+// GET HOST-NAME by hostname() Module
+console.log(os.hostname());
+
+// GET TOTAL-MEMORY by totalmem() Module
+console.log(os.totalmem());
+
+// GET FREE-MEMORY by freemem() Module
+console.log(os.freemem());
