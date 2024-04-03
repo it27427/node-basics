2a21a5c03813f22f363de6fe08f1275209584a1a
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-03 11:18:30 +0600

    added hostName and port

--- 01-node-built-in-modules/http.js
+++ 01-node-built-in-modules/http.js
@@ -5,7 +5,10 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = 8000;
+const hostName = '127.0.0.1';
 
 server.listen(PORT, () => {
-  console.log(`Server is successfully listening at: http://localhost:${PORT}`);
+  console.log(
+    `Server is successfully listening at: http://${hostName}:${PORT}`
+  );
 });
