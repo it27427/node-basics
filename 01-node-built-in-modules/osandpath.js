caba18f0905c2ed0e2da5197f942bc21c62d3f46
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-03 11:05:09 +0600

    extensionName and joinName from Path Module

--- 01-node-built-in-modules/osandpath.js
+++ 01-node-built-in-modules/osandpath.js
@@ -2,16 +2,28 @@
 const os = require('os');
 
 // GET USER-INFO by userInfo() Module
-console.log(os.userInfo());
+// console.log(os.userInfo());
 
 // GET HOME-DIRECTORY by homedir() Module
-console.log(os.homedir());
+// console.log(os.homedir());
 
 // GET HOST-NAME by hostname() Module
-console.log(os.hostname());
+// console.log(os.hostname());
 
 // GET TOTAL-MEMORY by totalmem() Module
-console.log(os.totalmem());
+// console.log(os.totalmem());
 
 // GET FREE-MEMORY by freemem() Module
-console.log(os.freemem());
+// console.log(os.freemem());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const path = require('path');
+
+// GET-EXTENSION NAME
+const extensionName = path.extname('index.html');
+// console.log(extensionName);
+
+const joinName = path.join(__dirname + '/views');
+console.log(joinName);
