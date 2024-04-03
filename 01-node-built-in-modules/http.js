199f0502418ce83bc5928dce3937deed505afb69
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-03 11:15:34 +0600

    add a message for listeing server

--- 01-node-built-in-modules/http.js
+++ 01-node-built-in-modules/http.js
@@ -4,4 +4,8 @@ const server = http.createServer((req, res) => {
   res.end('<h1>Hello from node server</h1>');
 });
 
-server.listen(8000);
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server is successfully listening at: http://localhost:${PORT}`);
+});
