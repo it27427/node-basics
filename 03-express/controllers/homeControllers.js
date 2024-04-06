86e3f30c40d4c5079ae14bcfa8967b8daee65439
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-06 11:31:28 +0600

    data-updated

--- 03-express/controllers/homeControllers.js
+++ 03-express/controllers/homeControllers.js
@@ -1,17 +1,17 @@
 const getControl = (req, res) => {
-  res.send('<h1>This is GET request from home route</h1>');
+  res.send('<h1>GET request from home route</h1>');
 };
 
 const postControl = (req, res) => {
-  res.send('<h1>This is POST request from home route</h1>');
+  res.send('<h1>POST request from home route</h1>');
 };
 
 const putControl = (req, res) => {
-  res.send('<h1>This is PUT request from home route</h1>');
+  res.send('<h1>PUT request from home route</h1>');
 };
 
 const deleteControl = (req, res) => {
-  res.send('<h1>This is DELETE request from home route</h1>');
+  res.send('<h1>DELETE request from home route</h1>');
 };
 
 module.exports = {
