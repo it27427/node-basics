6bc3734eb13dd0efa9c944a5d488419b6014269c
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-06 11:13:54 +0600

    DB_HOST to localhost

--- 03-express/server.js
+++ 03-express/server.js
@@ -5,11 +5,10 @@ const colors = require('colors');
 dotenv.config({ path: './config/dotenv.env' });
 
 const PORT = process.env.PORT || 3001;
-const hostName = process.env.DB_HOST;
 
 app.listen(PORT, () => {
   console.log(
-    `Server is successfully listening at: http://${hostName}:${PORT} in ${process.env.NODE_ENV} Mode`
+    `Server is successfully listening at: http://localhost:${PORT} in ${process.env.NODE_ENV} Mode`
       .brightYellow
   );
 });
