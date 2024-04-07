b1332f608836ae005541dd4e8a632042e3fea6a2
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-07 22:05:49 +0600

    add express-json and bodyParser

--- 03-express/app.js
+++ 03-express/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const homeRoutes = require('./routes/homeRoutes');
 const userRoutes = require('./routes/userRoutes');
