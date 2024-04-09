f44a00e7fc49c44e12949e2c4e3c30d998abecda
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-09 11:45:06 +0600

    Server is running

--- 04-area-calculator/app.js
+++ 04-area-calculator/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 app.use('/', (req, res) => {
   res.send('<h1>Express Area Calculator App</h1>');
--- 04-area-calculator/server.js
+++ 04-area-calculator/server.js
@@ -1,3 +1,14 @@
 const app = require('./app');
-
 const dotenv = require('dotenv');
+const colors = require('colors');
+
+dotenv.config({ path: './config/dotenv.env' });
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is successfully listening at: http://localhost:${PORT} in ${process.env.NODE_ENV} mode`
+      .bgMagenta
+  );
+});
