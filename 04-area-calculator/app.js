590914b130088c030b2e0a07b0c18b33e59b3ae7
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-09 11:56:36 +0600

    add view-pages

--- 04-area-calculator/app.js
+++ 04-area-calculator/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', (req, res) => {
-  res.send('<h1>Express Area Calculator App</h1>');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/' + 'index.html');
 });
 
 module.exports = app;
