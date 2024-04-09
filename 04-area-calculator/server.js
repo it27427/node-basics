0854968c0c93f88c1922b4cfd5a529113e319e0f
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-09 12:01:26 +0600

    server-listening-port updated

--- 04-area-calculator/server.js
+++ 04-area-calculator/server.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 
 dotenv.config({ path: './config/dotenv.env' });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(
