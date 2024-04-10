0366c43cac28a7e422698b39bc7f3a7ebd59053b
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-10 10:38:41 +0600

    body-parser added

--- 05-regular-expression/app.js
+++ 05-regular-expression/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 dotenv.config({ path: './config/dotenv.env' });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello from home route</h1>');
