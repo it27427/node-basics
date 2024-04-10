1dbc1b2a6acef10d37d9b86c2cc134621e4f1d45
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-10 10:56:14 +0600

    regular expression in express routing

--- 05-regular-expression/app.js
+++ 05-regular-expression/app.js
@@ -11,12 +11,12 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello from home route</h1>');
 });
 
-app.get('/products/:id([0-9]+)', (req, res) => {
+app.get('/products/:id([0-9]{3})', (req, res) => {
   // ROUTER-PARAMS
   res.send(`<h1>Product id : ${req.params.id}</h1>`);
 });
 
-app.get('/products/:title([a-zA-Z]{3})', (req, res) => {
+app.get('/products/:title([a-zA-Z0-9]+)', (req, res) => {
   res.send(`<h1>Product title : ${req.params.title}</h1>`);
 });
 
