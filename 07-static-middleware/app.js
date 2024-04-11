009457d1a61dee6996d2cebb90366c64c6688e82
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-11 16:26:53 +0600

    static-assets added

--- 07-static-middleware/app.js
+++ 07-static-middleware/app.js
@@ -12,19 +12,8 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// BAD-UEL MIDDLEWARE
-// app.use((req, res, next) => {
-//   res.send('<h2>Bad URL Request...</h2>');
-// });
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
 app.get('/', (req, res) => {
-  res.send('<h1>Hello From HOME</h1>');
+  res.sendFile(__dirname + '/views/' + 'index.html');
 });
 
 module.exports = app;
