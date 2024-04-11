d68d337b850b769b77530374e40b0237e9a037ef
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-11 15:30:42 +0600

    BAD-UEL MIDDLEWARE

--- 06-express-middleware/app.js
+++ 06-express-middleware/app.js
@@ -12,14 +12,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from home route</h1>');
+// BAD-UEL MIDDLEWARE
+app.use((req, res, next) => {
+  res.send('<h2>Bad URL Request...</h2>');
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from home route</h1>');
 });
 
 module.exports = app;
