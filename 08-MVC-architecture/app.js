e9967f0f983ed8484b4a976e7fd0e1a236c736a7
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-16 12:04:59 +0600

    app file added

---
+++ 08-MVC-architecture/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// CONFIGURED-DOTENV
+dotenv.config({ path: './config/dotenv.js' });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// GET-HOME
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Home Route</h1>');
+});
+
+module.exports = app;
