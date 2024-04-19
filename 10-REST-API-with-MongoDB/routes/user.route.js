6dfc617de86997300aa75ae9c68fc23fbe24d562
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-19 11:25:31 +0600

    updateUser controller import in user-router

--- 10-REST-API-with-MongoDB/routes/user.route.js
+++ 10-REST-API-with-MongoDB/routes/user.route.js
@@ -4,10 +4,12 @@ const {
   getAllUsers,
   getSingleUser,
   createUser,
+  updateUser,
 } = require('../controllers/user.controller');
 
 router.get('/users', getAllUsers);
 router.get('/users/:id', getSingleUser);
 router.post('/users', createUser);
+router.patch('/users/:id', updateUser);
 
 module.exports = router;
