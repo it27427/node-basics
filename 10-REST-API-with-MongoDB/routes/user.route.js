09e9cce2db4c9a00cf1946bfce22c7ece39acac5
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-19 11:17:20 +0600

    getSingleUser add in user-route

--- 10-REST-API-with-MongoDB/routes/user.route.js
+++ 10-REST-API-with-MongoDB/routes/user.route.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers } = require('../controllers/user.controller');
+const {
+  getAllUsers,
+  getSingleUser,
+} = require('../controllers/user.controller');
 
 router.get('/users', getAllUsers);
+router.get('/users/:id', getSingleUser);
 
 module.exports = router;
