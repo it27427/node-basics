dd94d1bb6f38446e42efbcf39d1ac0dc57e1ad19
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-19 15:20:08 +0600

    user update controller completed

--- 10-REST-API-with-MongoDB/controllers/user.controller.js
+++ 10-REST-API-with-MongoDB/controllers/user.controller.js
@@ -34,10 +34,17 @@ const createUser = async (req, res) => {
   }
 };
 
-const updateUser = (req, res) => {
-  res.status(200).json({
-    message: 'Updated User',
-  });
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ id: req.params.id });
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.phone = req.body.phone;
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 const deleteUser = async (req, res) => {
