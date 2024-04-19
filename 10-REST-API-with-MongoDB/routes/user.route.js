4e869f2e233b5602c0c82b3953f16322a61d4b17
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-19 11:19:31 +0600

    createUser imported in user-router

--- 10-REST-API-with-MongoDB/routes/user.route.js
+++ 10-REST-API-with-MongoDB/routes/user.route.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getAllUsers,
   getSingleUser,
+  createUser,
 } = require('../controllers/user.controller');
 
 router.get('/users', getAllUsers);
 router.get('/users/:id', getSingleUser);
+router.post('/users', createUser);
 
 module.exports = router;
