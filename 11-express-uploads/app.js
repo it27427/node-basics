ffa5a71aa66a8bde970cd9a4dd154558c18c58dd
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 11:57:06 +0600

    app file done

--- 11-express-uploads/app.js
+++ 11-express-uploads/app.js
@@ -3,5 +3,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 dotenv.config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// ERROR-NOT-FOUND
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Error 404! Page Not Found!!',
+  });
+});
+
+// INTERNAL-SERVER-ERROR
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something Broken!',
+  });
+});
+
+module.exports = app;
