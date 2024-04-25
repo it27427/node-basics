30862ee5d3f92dc4855e15a0a4f6b8d71ed557bf
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 13:41:50 +0600

    upload-controller views-path-source fixed

--- 11-express-uploads/controllers/upload.controller.js
+++ 11-express-uploads/controllers/upload.controller.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const getUpload = (req, res) => {
-  res.sendFile(path.join(__dirname + '/./views/uploads.html'));
+  res.sendFile(path.join(__dirname + '/../views/uploads.html'));
 };
 
 module.exports = {
