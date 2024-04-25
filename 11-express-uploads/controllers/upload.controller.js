559416729a6a49b9c3838eda8dc2bf4c0fb523e7
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 14:07:05 +0600

    Multer-DiskStorage

--- 11-express-uploads/controllers/upload.controller.js
+++ 11-express-uploads/controllers/upload.controller.js
@@ -1,10 +1,25 @@
 const path = require('path');
+const multer = require('multer');
+// const upload = multer({ dest: './public/images/uploads/' });
 
 const getUpload = (req, res) => {
   res.sendFile(path.join(__dirname + '/../views/uploads.html'));
 };
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 const uploadImage = (req, res) => {
+  const img = req.body.image;
   res.send('Image Uploaded');
 };
 
