48928b031b804e802eb62bdfb6043c62c0bcb7f9
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 11:53:20 +0600

    dotenv created and config

---
+++ 11-express-uploads/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
