3bb64c2791c617d7612e84c056ff8ba17776de87
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 14:47:15 +0600

    image-uploaded-successfully

--- 11-express-uploads/routes/upload.route.js
+++ 11-express-uploads/routes/upload.route.js
@@ -7,7 +7,7 @@ const { getUpload, uploadImage } = require('../controllers/upload.controller');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname + '/public/images/uploads/'));
+    cb(null, path.join(__dirname + '/../public/images/uploads/'));
   },
 
   filename: function (req, file, cb) {
