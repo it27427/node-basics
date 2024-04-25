9ec8a1a12fd077e09180868949733ed0d598edcb
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 14:40:36 +0600

    upload functions added in upload-route

--- 11-express-uploads/controllers/upload.controller.js
+++ 11-express-uploads/controllers/upload.controller.js
@@ -1,22 +1,8 @@
 const path = require('path');
-const multer = require('multer');
 
 const getUpload = (req, res) => {
   res.sendFile(path.join(__dirname + '/../views/uploads.html'));
 };
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname + '/public/images/uploads/'));
-  },
-
-  filename: function (req, file, cb) {
-    const name = Date.now() + '-' + file.originalname;
-    cb(null, name);
-  },
-});
-
-const upload = multer({ storage: storage });
 
 const uploadImage = (req, res) => {
   res.send('Image Uploaded');
--- 11-express-uploads/routes/upload.route.js
+++ 11-express-uploads/routes/upload.route.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
 const { getUpload, uploadImage } = require('../controllers/upload.controller');
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname + '/public/images/uploads/'));
+  },
+
+  filename: function (req, file, cb) {
+    const name = Date.now() + '-' + file.originalname;
+    cb(null, name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 router.get('/', getUpload);
-router.post('/', uploadImage);
+router.post('/', upload.single('image'), uploadImage);
 
 module.exports = router;
