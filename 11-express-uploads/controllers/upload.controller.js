d8e218e67df2b5ab247f333e90e23d24050344c6
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 14:51:07 +0600

    upload-controller done

--- 11-express-uploads/controllers/upload.controller.js
+++ 11-express-uploads/controllers/upload.controller.js
@@ -1,8 +1,22 @@
 const path = require('path');
+const multer = require('multer');
 
 const getUpload = (req, res) => {
   res.sendFile(path.join(__dirname + '/../views/uploads.html'));
 };
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname + '/../public/images/uploads/'));
+  },
+
+  filename: function (req, file, cb) {
+    const name = Date.now() + '-' + file.originalname;
+    cb(null, name);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const uploadImage = (req, res) => {
   res.send('Image Uploaded');
@@ -11,4 +25,5 @@ const uploadImage = (req, res) => {
 module.exports = {
   getUpload,
   uploadImage,
+  upload,
 };
